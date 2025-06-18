246473245bbf6ec9f19de516523c7644910f08b0
Marcio Ventura <marcio_10ventura@hotmail.com>
2025-06-18 09:24:01 -0300

    Alteração no projetinho

--- emanuel/src/App.java
+++ emanuel/src/App.java
@@ -3,61 +3,59 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        int n1, n2, operacao = 0;
-
-        System.out.println("informe o valor de n1");
-        n1 = in.nextInt();
-        System.out.println("informe o valor de n2");
-        n2 = in.nextInt();
-        while (operacao != 6) {
-            System.out.println("informe a operacao");
+        int n1 = 0, n2 = 0, operacao = 0;
+        while (true) {
+            System.out.println("\n=== Calculadora ===");
+            System.out.println("1 - Soma");
+            System.out.println("2 - Subtrção");
+            System.out.println("3 - Divisão");
+            System.out.println("4 - Multiplicação");
+            System.out.println("5 - Alterar valores");
+            System.out.println("6 - Sair");
+            System.out.println("Escolha a operação");
             operacao = in.nextInt();
-
-            if (operacao == 1) {
-                soma(n1, n2);
-
-            } else if (operacao == 2) {
-                subtracao(n1, n2);
-
-            } else if (operacao == 3) {
-                divisao(n1, n2);
-
-            } else if (operacao == 4) {
-                multiplicacao(n1, n2);
-
-            } else if (operacao == 5) {
-                System.out.println("informe o valor de n1");
-                n1 = in.nextInt();
-                System.out.println("informe o valor de n2");
-                n2 = in.nextInt();
+            if (operacao == 6) {
+                System.out.println("Saindo...");
+                break;
             }
 
+            if (operacao == 5) {
+                System.out.print("Informe o valor de n1:");
+                n1 = in.nextInt();
+                System.out.print("Informe o valor de n2:");
+                n2 = in.nextInt();
+
+            }
+
+            if (operacao >= 1 && operacao <= 4) {
+                System.out.print("Informe o valor de n1: ");
+                 n1 = in.nextInt();
+                System.out.print("Informe o valor de n2:");
+                 n2 = in.nextInt();
+
+                if (operacao == 3 && n2 == 0) {
+                    System.out.println("Erro: Não é possível fazer uma divisão por 0!");
+                    continue;
+                }
+                double resultado = calcular(n1, n2, operacao);
+                System.out.println("Resultado: " + resultado );
+
+            } else if (operacao!= 5) {
+                System.out.println("Operação inválida!");
+            } 
+            
         }
+        in.close();
     }
 
-    public static void soma(int n1, int n2) {
-        int soma = n1 + n2;
-        System.out.println("resultado: " + soma);
-
+    public static double calcular(int n1, int n2, int operacao){
+         switch (operacao) {
+            case 1: return n1 + n2;
+            case 2: return n1 - n2;
+            case 3: return (double) n1 / n2;
+            case 4: return n1 * n2;
+            default: return 0;
+            
     }
-
-    public static void subtracao(int n1, int n2) {
-        int subtracao = n1 - n2;
-        System.out.println("resultado: " + subtracao);
-
-    }
-
-    public static void divisao(double n1, double n2) {
-        double divisao = n1 / n2;
-        System.out.println("resultado: " + divisao);
-
-    }
-
-    public static void multiplicacao(int n1, int n2) {
-        int multiplicacao = n1 * n2;
-        System.out.println("resultado: " + multiplicacao);
-
-    }
-
-
+  }
 }
